7500451467cad86ed346a4446b66a2162127c84d
Roberto Blanco <86691875+RobertoBlanco20@users.noreply.github.com>
2022-07-26 13:33:07 -0300

    box ready

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const foreignCurrency = document.querySelector('.second-currency');
 const boxs = document.querySelector('.boxs');
 const btnConversor = document.querySelector('#btn');
 
+let boxArray = [];
+
 
 /* VARIABLES INFORMACION */
 let amountNum;
@@ -39,7 +41,6 @@ function conversor(e){
     if(base.value !== foreignCurrency.value){
         fetch(`https://api.exchangerate.host/convert?from=${base.value}&to=${foreignCurrency.value}`)
             .then( respuesta => {
-
                 return respuesta.json();
             } )
             .then( datos => {
@@ -56,37 +57,48 @@ function conversor(e){
 
 
 function printBase(firstResult){
-
+    
+    const theBox = document.querySelector('.boxs > div');
+    
+         if(theBox != null){
+            theBox.remove()
+        }
+    
     let divOne = document.createElement('div');
     divOne.classList.add('card', 'card-one', 'col-12', 'col-md-5', 'mt-5')
 
     divOne.innerHTML = `<div class="card-body mt-4">
     <h5 class="card-title text-center fs-1 mt-5">$ <span>${firstResult}</span></h5>
-    <h6 class="card-subtitle mb-2 text-muted text-center fs-3">MONEDA</h6>
+    <h6 class="card-subtitle mb-2 text-muted text-center fs-3">${base.value}</h6>
   </div>`
 
-    divOne.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(../images/${base.value}.jpg)`
+    divOne.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(../images/${base.value}.jpg)`;
 
-    boxs.appendChild(divOne)  
+        boxs.appendChild(divOne)  
+
     }
 
 
 function printForeign( secondResult ){
 
-     let divTwo = document.createElement('div');
+    const theBox = document.querySelector('.boxs .card-two');
+    
+    if(theBox != null){
+       theBox.remove()
+   }
+
+    let divTwo = document.createElement('div');
     divTwo.classList.add('card', 'card-two', 'col-12', 'col-md-5', 'offset-md-2', 'mt-5');
 
     divTwo.innerHTML = `<div class="card-body mt-4">
     <h5 class="card-title text-center fs-1 mt-5">$ <span>${Math.round(secondResult * 100) / 100
     }</span></h5>
-    <h6 class="card-subtitle mb-2 text-muted text-center fs-3">MONEDA</h6>
+    <h6 class="card-subtitle mb-2 text-muted text-center fs-3">${foreignCurrency.value}</h6>
  </div>`;
 
-   divTwo.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(../images/${foreignCurrency.value}.jpg)`;
+    divTwo.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(../images/${foreignCurrency.value}.jpg)`;
 
-    boxs.appendChild(divTwo)  
-
-
+    boxs.appendChild(divTwo)
 }
 
 function resetAmount(){
